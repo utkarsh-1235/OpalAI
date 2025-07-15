eb76350941bb6adea8770e56dcd081a18fa2feca
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-07-15 22:35:01 +0530

    Adding clerk auth

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+    return(
+        <div className="p-4">
+            <Button> CLick Me</Button>
+        </div>
+    )
+}
+
+export default Home;
