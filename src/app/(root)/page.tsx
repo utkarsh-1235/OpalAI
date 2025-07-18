a46fd56f31908539ee45e16b8d44130b26c8fcfa
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-07-18 23:35:20 +0530

    Adding storeModals/modals in component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+// import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetUpPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if(!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen])
     return(
         <div className="p-4">
-            <Modal title="test" description="test description" isOpen onClose={() => {}}>
-                Children
-            </Modal>
+           Root Page
         </div>
     )
 }
