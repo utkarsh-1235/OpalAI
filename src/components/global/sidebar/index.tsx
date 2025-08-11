18dc4ff3589eb54fabc47490ab36a5f4ff05dccc
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-08-11 22:28:12 +0530

    Adding sidebar component and dashboard/workspaceId

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { Select, SelectContent, SelectGroup, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { getWorkSpaces } from "@/actions/workspace";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
+import { useQueryData } from "@/hooks/useQueryData";
+import { WorkspaceProps } from "@/types";
 import { useRouter } from "next/navigation";
 
 type Props = {
@@ -9,11 +12,16 @@ type Props = {
 }
 const Sidebar = ({activeWorkspaceId}: Props) => {
     const router = useRouter();
+
+    const {data, isFetched} = useQueryData(['user-workspaces'], getWorkSpaces);
+
+    const { workspace = [], members = [] } = (data as WorkspaceProps)?.data || {};
+
     const onChangeActiveWorkSpace = (value: string) => {
         router.push(`/dashboard/${value}`)
     }
 return (
-<div className="bg-white flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden">
+<div className="bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden">
     <div className="bg-[#111111] p-4 gap-2 justify-center items-center mb-4 absolute top-0 right-0">
         <div className=" flex text-2xl font-bold bg-gradient-to-r from-cyan-400 to-purple-500 bg-clip-text text-transparent">
            VisionAI
@@ -30,6 +38,20 @@ return (
             <SelectGroup>
                 <SelectLabel> Workspaces</SelectLabel>
                 <Separator/>
+                {workspace.map((workspace) => (
+                    <SelectItem
+                    key={workspace.id}
+                    value={workspace.id}>
+                      {workspace.name}
+                    </SelectItem>
+                ))}
+                {members.length > 0 && members?.map((member) => member.Workspace && (
+                     <SelectItem
+                      value={member.Workspace.id}
+                      key={member.Workspace.id}>
+                        {member.Workspace.name}
+                      </SelectItem>
+                ))}
             </SelectGroup>
         </SelectContent>
     </Select>
